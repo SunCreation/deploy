3a467ab099e416cd3eb4fb5ada0082243719ef07
SunCreation <cake0103@nate.com>
2022-06-10 20:29:09 +0900

    creating draft of deployment

--- setup.py
+++ setup.py
@@ -1,27 +1,37 @@
+import sys
 from setuptools import setup, find_packages
 
 setup_requires = [
     ]
 
 install_requires = [
-    'django==4.0',
-    'markdown==3.1',
+    # 'django==4.0',
+    # 'markdown==3.1',
     'pyyaml==6.0',
-    'pillow==9.1.1',
-    'lxml==4.8.3',
-    'beautifulsoup4==4.9.0',
+    # 'pillow==9.1.1',
+    # 'lxml==4.8.3',
+    # 'beautifulsoup4==4.9.0',
     ]
 
+tests_require = [
+    'nose==1.3.7',
+    'coverage==6.4.1',
+]
+
+if sys.version_info < (2,7):
+    test_require.append('unittest2==0.5.1')
+
 dependency_links = [
-    'git+https://github.com/django/django.git@stable/1.6.x#egg=Django-1.6b4',
     ]
 
 setup(
-    name='Flowdas-Books',
-    version='0.5.0',
-    description='Flowdas Books',
-    author='Flowdas',
-    author_email='spammustdie@flowdas.com',
+    name='aiffel-git',
+    version='0.1.0a',
+    license='MIT',
+    description='easy git push system',
+    long_description=open('README.md').read(),
+    author='hchang',
+    author_email='cake0103@nate.com',
     packages=find_packages(),
     install_requires=install_requires,
     setup_requires=setup_requires,
@@ -29,9 +39,17 @@ setup(
     scripts=['manage.py'],
     entry_points={
         'console_scripts': [
-            'publish = flowdas.books.script:main',
-            'scan = flowdas.books.script:main',
-            'update = flowdas.books.script:main',
+            'publish = bin:main',
+            'scan = bin:main',
+            'update = bin:main',
             ],
         },
+    extras_require={
+        'test': tests_require,
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ]
     )
